5e46f1c9894b2a853dea012a767650a1c724e50f
jjordan-quantum <constant.space@gmail.com>
2021-05-19 12:34:13 -0700

    added server status to / route, added routest to set/check trigger levels for arbitrary actions

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 
 #key = os.getenv("PRIVATE_KEY")
 
-@app.route('/')
-def home():
-    return routes.get_status()
 
-# Routes for Asynchronous Operations
+@app.route('/')
+def status():
+    status = routes.get_status()
+    msg = '<html>'
+    for item in status:
+        msg += '<p>' + item + '</p>'
+    msg += '</html>'
+    return msg
+
+
+# Routes for Control of Asynchronous Operations
 #-------------------------------------------
+
 
 @app.route('/start')
 def start_process():
@@ -29,7 +37,31 @@ def pause_process():
 def unpause_process():
     return routes.unpause_process()
 
+
 # Routes for Specific functions
 #-------------------------------------------
+
+
+@app.route('/get_trigger_amount')
+def get_trigger_amount():
+    trigger_amount = routes.get_trigger_amount()
+    return f'Trigger amount: {trigger_amount}'
+
+@app.route('/set_trigger_amount')
+def set_trigger_amount():
+    # Parameter for testing (parameters will be part of the route in future)
+    amount = 100*10**18  # 100 tokens in smallest units
+
+    trigger_amount = routes.set_trigger_amount(amount)
+    return f'Trigger amount set to {trigger_amount}'
+
+@app.route('/get_token_balance')
+def get_token_balance():
+    # Parameters for testing (parameters will be part of the route in future)
+    token = '0xFB52FC1f90Dd2B070B9Cf7ad68ac3d68905643fa'  # Sea Token
+    account = '0xaf72Fb3310561C0826fdF852c05bC50BF54989cd'  # Sea Token Charity Wallet
+
+    balance = routes.get_token_balance(token=token, account=account)
+    return f'Balance for the given account and token is: {balance}'
 
 
--- routes.py
+++ routes.py
@@ -7,16 +7,28 @@ import asyncio
 import lib.abis as abis
 
 # global variables
+
+# web3 variables
 web3 = None
 global_provider = 'https://bsc-dataseed.binance.org/'
 web3_count = 0
+
+# async command flags
 pause = False
 kill = False
 running = False
 DELAY = 1 # in seconds
 
+# trigger amount for async loop to execute functions
+trigger_amount = 200000000000000000000000000
+SEA_TOKEN_TOTAL_SUPPLY = 200000000000000000000000000
+
 # for testing
 loop_count = 0
+
+# Addresses for SEA TOKEN
+SEA_TOKEN_ADDRESS = '0xFB52FC1f90Dd2B070B9Cf7ad68ac3d68905643fa'
+SEA_TOKEN_CHARITY_WALLET = '0xaf72Fb3310561C0826fdF852c05bC50BF54989cd'
 
 # note: PancakeSwap V1 addresses
 MASTERCHEF_ADDRESS = '0x73feaa1eE314F8c655E354234017bE2193C9E24E'
@@ -43,16 +55,34 @@ with open("lib/pools.json", 'r') as read_file:
 with open("lib/tokens.json", 'r') as read_file:
     tokens = json.load(read_file)
 
-# Functions for basic command routes
+# Functions for process control command routes
 #----------------------------------------------
 
+
 def get_status():
+
     global loop_count
-    return f'Total loop count: {loop_count}'
+    global pause
+    global running
+    global web3_count
+    global trigger_amount
+    balance = _get_token_balance(token=SEA_TOKEN_ADDRESS, account=SEA_TOKEN_CHARITY_WALLET)
+    balance_over_trigger_amount = balance >= trigger_amount
+    return [f'Current Server Status: ',
+            f'Running: {running}',
+            f'Paused: {pause}',
+            f'Total loop count: {loop_count}',
+            f'Total web3 count: {web3_count}',
+            f'Trigger amount: {trigger_amount}',
+            f'Balance of charity wallet: {balance}',
+            f'Current balance greater than trigger amount: {balance_over_trigger_amount}']
+
 
 def start_process():
+
     global running
     if not running:
+        _reset_trigger_amount()
         running = True
         thread_1 = Thread(target=runner)
         thread_1.start()
@@ -60,7 +90,9 @@ def start_process():
     else:
         return 'Process already running.'
 
+
 def stop_process():
+
     global running
     global kill
     if running:
@@ -69,7 +101,9 @@ def stop_process():
     else:
         return 'Process not running.'
 
+
 def pause_process():
+
     global pause
     if pause:
         return 'Process already paused.'
@@ -77,7 +111,9 @@ def pause_process():
         pause = True
         return 'Pausing process....'
 
+
 def unpause_process():
+
     global pause
     if not pause:
         return 'Process is not paused.'
@@ -85,10 +121,46 @@ def unpause_process():
         pause = False
         return 'Unpausing process...'
 
-# Async functions implemented via threads
+# Functions for interacting with async loop
 #----------------------------------------------
 
+
+def get_trigger_amount():
+
+    global trigger_amount
+    return trigger_amount
+
+
+def set_trigger_amount(new_trigger_amount):
+
+    global trigger_amount
+    trigger_amount = new_trigger_amount
+    return trigger_amount
+
+def _reset_trigger_amount():
+    global trigger_amount
+    global SEA_TOKEN_TOTAL_SUPPLY
+    trigger_amount = SEA_TOKEN_TOTAL_SUPPLY
+
+
+# Functions for account / smart contract info routes
+#----------------------------------------------
+
+
+
+def get_token_balance(token, account):
+
+    #print(f'In get_token_balance with params: \n{token}, \n{account}')
+    balance = _get_token_balance(token=token, account=account)
+    return balance
+
+
+# Async loop implemented via threads
+#----------------------------------------------
+
+
 def runner():
+
     global pause
     global kill
     global running
@@ -105,8 +177,10 @@ def runner():
             break
         time.sleep(DELAY)
 
+
 # Web3 and smart contract functions
 #----------------------------------------------
+
 
 def connect_web3(_provider=None):
 
@@ -131,19 +205,29 @@ def connect_web3(_provider=None):
     web3_count += 1
     global_provider = provider
 
+
 def get_transactions(from_block, to_block, sender=None, recipient=None, topics=[]):
 
-    if not web3:
-        connect_web3()
-    if not web3.isConnected():
-        connect_web3()
+    _check_web3()
     global web3_count
     # query over block range
     # stop on failure
     # write results to json file
     return f'get_transactions() from: {from_block} to: {to_block}\n Global web3 count: {web3_count}'
 
+
+def _get_token_balance(token, account):
+
+    #print(f'In _get_token_balance with params: {token}, {account}')
+    _check_web3()
+    assert web3.isAddress(token) and web3.isAddress(account), 'Token and account must be addresses'
+    token_contract = _get_contract(web3.toChecksumAddress(token), erc20_abi)
+    return token_contract.functions.balanceOf(web3.toChecksumAddress(account)).call()
+
+
 def _select_contract(contract):
+
+    _check_web3()
     if contract == 'masterchef':
         return _get_contract(MASTERCHEF_ADDRESS, masterchef_abi)
     elif contract == 'factory_v1':
@@ -163,9 +247,18 @@ def _select_contract(contract):
         return _get_contract(address, abi)
     return 'Specified contract not found'
 
-def _get_contract(contract_address, contract_abi):
+
+def _check_web3():
+
+    #print(f'In _check_web3')
     if not web3:
         connect_web3()
     if not web3.isConnected():
         connect_web3()
+
+
+def _get_contract(contract_address, contract_abi):
+
+    # assumes web3 has already been checked
+    #print(f'In _get_contract with params: {contract_address}, {contract_abi}')
     return web3.eth.contract(address=contract_address, abi=contract_abi)
